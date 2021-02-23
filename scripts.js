bffaae53101f422315478ae0ac062dce05af94f8
Joao Victor <joao.gomes31@fatec.sp.gov.br>
2021-02-23 15:32:56 -0300

    Aula 2

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const modal = {
+    open() {
+        // Abrir modal
+        // Adicionar a class active ao modal
+        document
+            .querySelector('.modal-overlay')
+            .classList.add('active');
+        },
+    close() {
+        // Fechar o modal
+        // remover a class active do modal
+        document
+            .querySelector('.modal-overlay')
+            .classList.remove('active');
+    }
+}
